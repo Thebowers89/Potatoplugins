808583c3e36461723d3feeab66fdeb4ca10a1931
BuildTools <unconfigured@null.spigotmc.org>
2017-03-30 01:24:28 -0500

    updated 3.30.17 1:24am

--- src/PotatoBlocker/Blockone.java
+++ src/PotatoBlocker/Blockone.java
@@ -1,10 +1,10 @@
 package PotatoBlocker;
 
-        import org.bukkit.ChatColor;
-        import org.bukkit.command.Command;
-        import org.bukkit.command.CommandExecutor;
-        import org.bukkit.command.CommandSender;
-        import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Blockone
         implements CommandExecutor
--- src/PotatoBlocker/Blocktwo.java
+++ src/PotatoBlocker/Blocktwo.java
@@ -1,6 +1,5 @@
 package PotatoBlocker;
 
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
