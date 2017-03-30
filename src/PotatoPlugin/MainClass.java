b1e0b5af9752647e23af431e1f2752b21f4796e5
BuildTools <unconfigured@null.spigotmc.org>
2017-03-30 15:15:35 -0400

    Tweaked the MainClass

--- src/PotatoPlugin/MainClass.java
+++ src/PotatoPlugin/MainClass.java
@@ -3,21 +3,30 @@ package PotatoPlugin;
 import PotatoBlocker.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainClass
-        extends JavaPlugin {
+public class MainClass extends JavaPlugin {
     public void onEnable() {
         registerCommands();
     }
 
     public void onDisable() {
+
     }
 
     private void registerCommands() {
+        registerBlocker();
+        registerVote();
+    }
+
+    private void registerVote() {
         getCommand("vote").setExecutor(new VoteCommand());
         getCommand("addurl").setExecutor(new AddUrlCommand());
+    }
+
+    private void registerBlocker() {
         getCommand("plugins").setExecutor(new Blockone());
         getCommand("plugin").setExecutor(new Blocktwo());
         getCommand("pl").setExecutor(new Blockthree());
         getCommand("?").setExecutor(new Blockfour());
     }
+
 }
